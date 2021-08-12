f0f8257a8f0909a516037715cedc8e9f1af2e264
tsm <aleksandrs.jakobsons@foodunion.com>
2021-08-12 15:58:37 +0300

    config and start

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+	define('system','/var/www/vhosts/foodunion.lv/tsm.foodunion.lv/system/');
+	define('catalog','/var/www/vhosts/foodunion.lv/tsm.foodunion.lv/catalog/');
+  
---
+++ system/start.php
@@ -0,0 +1,9 @@
+<?php
+	//starting app
+	require_once('/var/www/vhosts/foodunion.lv/tsm.foodunion.lv/config.php');
+
+	//router
+	require system.'router.php';
+
+	$router = new Route;
+	$router->getRoute();
